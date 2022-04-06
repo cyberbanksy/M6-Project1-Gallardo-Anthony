a19f01396b2725ce272a99e9d3a2bf4eb617b568
Anthony Gallardo <agallar3@uci.edu>
2022-04-06 14:20:38 -0500

    commit 2.1

---
+++ src/main/java/com/company/Person.java
@@ -0,0 +1,18 @@
+package com.company;
+
+public class Person {
+    // state or properties
+    private String name;
+    private double salary;
+    private double earnedIncome;
+
+    public void setName(String name){
+        this.name = name;
+    }
+    public void setSalary(double salary){
+        this.salary = salary;
+    }
+    public void setEarnedIncome(double earnedIncome){
+        this.earnedIncome = earnedIncome;
+    }
+}
