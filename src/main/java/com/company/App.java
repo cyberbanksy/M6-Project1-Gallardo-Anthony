18b37bb18e5b10654d13b2566cd86c91db4af27a
Anthony Gallardo <agallar3@uci.edu>
2022-04-10 22:32:59 -0500

    commit 3

--- src/main/java/com/company/App.java
+++ src/main/java/com/company/App.java
@@ -2,6 +2,16 @@ package com.company;
 
 public class App {
     public static void main(String[] args) {
-        Person
-    }
+        Person jon = new Person();
+        jon.setName("Jon");
+        jon.setSalary(75000);
+        jon.setEarnedIncome(25000);
+
+        Person ron = new Person();
+        ron.setName("ron");
+        jon.setSalary(95000);
+        jon.setEarnedIncome(36000);
+
+
+        }
 }
